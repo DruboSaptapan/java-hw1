e70ec8aeabda8253bf85d08271830656a7936dad
Saptapan Barua <saptapan2017@gmail.com>
2022-07-26 23:51:38 +0600

    multilevelInheritance added

--- multilevelInherit.java
+++ multilevelInherit.java
@@ -1,25 +1,42 @@
-class student1{ //super class
-    void khata1(){
-        System.out.println("Load-shedding means the discontinuation of the supply of electricity.");
+class Student{ //super class
+    String name;
+    int roll;
+    void getData(){
+        name = "Saptapan Barua";
+        roll = 494303;
+    }
+    void display(){
+        System.out.println("Inside student class");
+        System.out.println("Name: "+name);
+        System.out.println("Roll: "+roll);
     }
 }
-class student2 extends student1{ // sub class extends subper class
-    void khata2(){
-        student1 S = new student1();
-        S.khata1();
-        System.out.println("Load-shedding means the discontinuation of the ");
+class Exam extends Student{ // sub class
+    float mark;
+    void getData(){
+        mark = 96.34f;
+    }
+    void display(){
+        System.out.println("Inside exam class");
+        System.out.println("Mark: "+mark);
     }
 }
-class student3 extends student2{ // sub class extends subper class
-    void khata3(){
-        student2 S = new student2();
-        S.khata2();
-        System.out.println("Load-shedding means ");
+class Result extends Exam{ // another sub class
+    Student S = new Student();
+    Exam E = new Exam();
+    void getData(){
+        S.getData();
+        E.getData();
+    }
+    void display(){
+        S.display();
+        E.display();
     }
 }
 public class multilevelInherit {
     public static void main(String[] args) {
-        student3 S = new student3();
-        S.khata3();
+        Result R = new Result();
+        R.getData();
+        R.display();
     }
 }
