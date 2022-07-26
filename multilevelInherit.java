ffb20c38de4f4b02fdacd1d2a697125ec11e7b2c
Saptapan Barua <saptapan2017@gmail.com>
2022-07-26 23:11:28 +0600

    multilevelInheritance added

--- multilevelInherit.java
+++ multilevelInherit.java
@@ -1,16 +1,16 @@
-class student1{
+class student1{ //super class
     void khata1(){
         System.out.println("Load-shedding means the discontinuation of the supply of electricity.");
     }
 }
-class student2 extends student1{
+class student2 extends student1{ // sub class extends subper class
     void khata2(){
         student1 S = new student1();
         S.khata1();
         System.out.println("Load-shedding means the discontinuation of the ");
     }
 }
-class student3 extends student2{
+class student3 extends student2{ // sub class extends subper class
     void khata3(){
         student2 S = new student2();
         S.khata2();
